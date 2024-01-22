b1d5be3cf957e4703d9efbdd71310bbbf9501cf4
José M. Zavala González <jose.m.zavala.gonzalez@gmail.com>
2024-01-21 23:58:00 -0400

    Initial import

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime, timezone, timedelta
 
 def get_atc_now():
@@ -15,3 +16,29 @@ def get_atc_now():
     ast_time = utc_now.replace(tzinfo=timezone.utc).astimezone(ast_tz)
 
     return ast_time
+
+def get_all_objects(s3_client, prefix=None):
+    ''' Get list of all objects in the bucket '''
+    paginator = s3_client.get_paginator('list_objects_v2')
+    if prefix is None:
+        pages = paginator.paginate(Bucket='archiva-apagones')
+    else:
+        pages = paginator.paginate(Bucket='archiva-apagones', Prefix=prefix)
+
+    for page in pages:
+        if 'Contents' in page:
+            for obj in page['Contents']:
+                yield obj
+
+def download_object(s3_manager, obj_key, local_path, verbose=False):
+    ''' Download an object from S3 to a local path '''
+    file_directory = os.path.dirname(local_path)
+    os.makedirs(file_directory, exist_ok=True)
+    if verbose:
+        print(f'Downloading {obj_key} to {local_path}')
+    # s3_client.download_file('archiva-apagones', obj_key, local_path) # when using just a client
+    s3_manager.download(
+        bucket='archiva-apagones',
+        key=obj_key,
+        fileobj=local_path,
+    )
